caec414e96c442a845eb22226b43a90462ba2aad
KnotFaries <205455908+KnotFaries@users.noreply.github.com>
2026-04-12 15:22:57 -0400

    Move to Test
    Moving test

--- SoundGenerationClasses/WaveTable.py
+++ SoundGenerationClasses/WaveTable.py
@@ -9,6 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+
 def attack (signal, fade_length = 1000):
     """
 
@@ -149,32 +150,3 @@ def generate_wavetable(frequency, durration, waveform, sample_rate = 44100):
     
       
     return output
-    
-sample_rate = 44100
-frequency = 440
-durration = 1
-waveform = np.sin
-
-c = generate_wavetable(261.63, durration, waveform)
-plt.plot(c[-6000:])
-"""
-d = generate_wavetable(293.66, durration, waveform)
-e = generate_wavetable(329.63, durration, waveform)
-f = generate_wavetable(349.23, durration, waveform)
-g = generate_wavetable(392.00, durration, waveform)
-a = generate_wavetable(440, durration, waveform)
-b = generate_wavetable(493.88, durration, waveform)
-c5 = generate_wavetable(523.24, durration, waveform)
-
-
-
-sd.play(c)
-sd.wait()
-sd.play(e)
-sd.wait()
-sd.play(g)
-sd.wait()
-
-sd.play(c)
-sd.play(e)
-sd.play(g)"""
